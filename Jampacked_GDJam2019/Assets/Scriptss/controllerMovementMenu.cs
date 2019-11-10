8f70e2f4ace9f5ff5301453256b6752e4af6710c
Leo Buono <leo.buono@ontariotechu.net>
2019-11-10 14:50:27 -0500

    Added 4 Controller support to main Menu
    Background Gameplay is a work in progress

--- Jampacked_GDJam2019/Assets/Scriptss/controllerMovementMenu.cs
+++ Jampacked_GDJam2019/Assets/Scriptss/controllerMovementMenu.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class controllerMovementMenu : MonoBehaviour
 {
-    string aButton = "P1A";
-    string verticalController = "P1Vertical";
+    string[] aPress = new string[] { "P1A", "P2A", "P3A", "P4A", };
+    string[] verticalController = new string[] { "P1Vertical", "P2Vertical", "P3Vertical", "P4Vertical" };
     int cursorPlace = 0;
     public Text[] buttons = new Text[3];
     public GameObject MenuPanel;
@@ -23,52 +23,59 @@ public class controllerMovementMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetAxis(verticalController) < 0.5f && (Input.GetAxis(verticalController) > -0.5f)) && reset != true)
+        for (int i = 0; i < verticalController.Length; i++)
         {
-            reset = true;
-        }
-        if (reset)
-        {
-            if (Input.GetAxis(verticalController) > 0.5f)
+
+
+            if ((Input.GetAxis(verticalController[i]) < 0.5f && (Input.GetAxis(verticalController[i]) > -0.5f)) && reset != true)
             {
-                if (cursorPlace != 2)
+                reset = true;
+            }
+            if (reset)
+            {
+                if (Input.GetAxis(verticalController[i]) > 0.5f)
                 {
-                    buttons[cursorPlace].color = Color.black;
-                    cursorPlace++;
-                    buttons[cursorPlace].color = Color.red;
-                    reset = false;
+                    if (cursorPlace != 2)
+                    {
+                        buttons[cursorPlace].color = Color.black;
+                        cursorPlace++;
+                        buttons[cursorPlace].color = Color.red;
+                        reset = false;
+                    }
+                }
+                else if (Input.GetAxis(verticalController[i]) < -0.5f)
+                {
+
+                    if (cursorPlace != 0)
+                    {
+                        buttons[cursorPlace].color = Color.black;
+                        cursorPlace--;
+                        buttons[cursorPlace].color = Color.red;
+                        reset = false;
+                    }
+
                 }
             }
-            else if (Input.GetAxis(verticalController) < -0.5f)
-            {
-
-                if (cursorPlace != 0)
-                {
-                    buttons[cursorPlace].color = Color.black;
-                    cursorPlace--;
-                    buttons[cursorPlace].color = Color.red;
-                    reset = false;
-                }
-
-            }
         }
-
-        if (Input.GetButtonUp(aButton)) 
+        for (int i = 0; i < aPress.Length; i++)
         {
-            switch (cursorPlace)
+            if (Input.GetButtonUp(aPress[i]))
             {
-                case 0:
-                    SceneManager.LoadScene("CharacterSelect");
-                    break;
-                case 1:
-                    MenuPanel.SetActive(false);
-                    HelpPanel.SetActive(true);
-                    break;
-                case 2:
-                    Application.Quit();
-                    break;
-                default:
-                    break;
+                switch (cursorPlace)
+                {
+                    case 0:
+                        SceneManager.LoadScene("CharacterSelect");
+                        break;
+                    case 1:
+                        MenuPanel.SetActive(false);
+                        HelpPanel.SetActive(true);
+                        break;
+                    case 2:
+                        Application.Quit();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 

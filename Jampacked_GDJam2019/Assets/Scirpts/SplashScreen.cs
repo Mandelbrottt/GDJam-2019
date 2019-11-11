ae6468ec3ae16c5a0310cc598f6a5a4adb3d23f3
Mat <mathew.kostrzewa@ontariotechu.net>
2019-11-10 20:43:08 -0500

    Added splash screen and fixed audio in title screen

---
+++ Jampacked_GDJam2019/Assets/Scirpts/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    private float countDown = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countDown < 0.0f)
+            SceneManager.LoadScene("TitleScreen");
+
+        countDown -= Time.deltaTime;
+    }
+}
